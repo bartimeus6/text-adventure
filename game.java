8d9b76116e536df52df06fd2bca43a1ae550ebc0
bartimeus6 <43414325+bartimeus6@users.noreply.github.com>
2019-01-19 11:04:06 +0100

    added help command

--- game.java
+++ game.java
@@ -4,7 +4,7 @@ public class game{
     public static void main(String[] args) {
 	tools.tool t = new tool();
 	player plyr = new player();
-	System.out.println("System: Hello dear player, and welcome to text-adveture!\n        To start playing just type a command!");
+	System.out.println("System: Hello dear player, and welcome to text-adveture!\n        To start playing just type a command! \n        (type help for a list of commands)");
         while (true) {
             String cmd = t.nextLine();
             switch (cmd) {
@@ -17,6 +17,10 @@ public class game{
 		case "quit":
 		    System.out.println("System: See you later!");
 		    System.exit(0);
+		case "help":
+			System.out.println("System: Available commands are:");
+			System.out.println("        \'new character\'; \'stats\'; \'quit\'");
+			break;
                 default:
                     System.out.println("System: Unknown command!");
             }
